45954fe7d03307a82f2c38d24cc86e59447101b9
xcellardoor <sam@samcater.com>
2017-06-13 22:58:26 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,12 @@
+//Main package
+package main
+
+import "fmt"
+
+//Add a function that reads the configuration file
+
+//Add a function that takes an array of
+
+func main() {
+	fmt.Println("Hello, this compiles")
+}
